5c4561910b1393376aaef21034204c8786685a87
Your Name <pravin.gowtham@gmail.com>
2026-07-30 11:04:46 +0530

    first commit

---
+++ src/main/java/com/example/ArithmeticOperator.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class ArithmeticOperator {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+
+        System.out.println("First Number : " + a);
+        System.out.println("Second Number: " + b);
+
+        // Arithmetic Operations
+        System.out.println("Addition       = " + (a + b));
+        System.out.println("Subtraction    = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division       = " + (a / b));
+        System.out.println("Modulus        = " + (a % b));
+    }
+}
